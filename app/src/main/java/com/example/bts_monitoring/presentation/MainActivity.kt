33d970708c438d58392b95dd4bd6f933e4fd08cf
Alexander <alexandr.bechko@gmail.com>
2023-01-11 13:35:41 +0300

    setup project

---
+++ app/src/main/java/com/example/bts_monitoring/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bts_monitoring.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bts_monitoring.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

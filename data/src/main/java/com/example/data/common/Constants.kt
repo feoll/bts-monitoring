e42381b2201a3e3acfbad408d90e5d94458c09ec
Alexander <alexandr.bechko@gmail.com>
2023-04-20 23:24:45 +0300

    hide api key

--- data/src/main/java/com/example/data/common/Constants.kt
+++ data/src/main/java/com/example/data/common/Constants.kt
@@ -1,5 +1,7 @@
 package com.example.data.common
 
+import com.example.data.BuildConfig
+
 const val BASE_URL = "https://belarusborder.by/"
-const val ZONES_TOKEN = "bts47d5f-6420-4f74-8f78-42e8e4370cc4"
-const val ZONE_QUEUE_TOKEN = "test"
+const val ZONES_TOKEN = BuildConfig.ZONES_TOKEN
+const val ZONE_QUEUE_TOKEN = BuildConfig.ZONE_QUEUE_TOKEN
